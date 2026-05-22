928988bd7591a1c0cccdcf2a891790fcdebbb4e9
Amiir25 <amirsadik636@gmail.com>
2026-05-22 16:09:22 +0300

    buildin the foundations

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query"
+import { getWeather } from "./api"
+
+const App = () => {
+
+  const { data } = useQuery({
+    queryKey: ["weather"],
+    queryFn: () => getWeather({ lat: 50, lon: 50 }),
+  })
+
+  return (
+    <div>{JSON.stringify(data)}</div>
+  )
+}
+
+export default App
